2170726cd4ae59be000a6df99d8e1295a2d19f3d
reiyitan <tanreiyi@gmail.com>
2023-06-27 23:29:23 -0500

    firebase config

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,14 @@
-import { ChatPanel } from "../components";
+import { ChatPage } from "../pages";
 import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "../env";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export const App = () => {
     return (
-        <ChatPanel />
+        <ChatPage />
     );
 }
