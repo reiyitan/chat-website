08419e7ebae20cb085d8ffbd3d13a4a59cb481f5
reiyitan <tanreiyi@gmail.com>
2023-07-02 13:04:46 -0500

    docs

--- src/components/TextForm/TextForm.js
+++ src/components/TextForm/TextForm.js
@@ -1,6 +1,12 @@
 import React from "react"; 
 import "./TextForm.css";
 
+/**
+ * Component for getting input from a user. 
+ * 
+ * @param setValue - Setter function to update a state variable to the current value of the input element.
+ * @param label - The label to display above the text input. 
+ */
 export const TextForm = ({setValue, label}) => {
     const handleInput = (e) => {
         e.preventDefault();
