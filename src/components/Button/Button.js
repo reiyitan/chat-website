328e4419459067eab3f00ee5ff6bfccf1676cbdb
reiyitan <tanreiyi@gmail.com>
2023-07-02 13:02:35 -0500

    docs

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,12 @@
 import React from "react"; 
 import "./Button.css";
 
+/**
+ * A custom button component.
+ * 
+ * @param onClick - Callback function to be passed to the button's onClick.
+ * @param text - The text to display on the button. 
+ */
 export const Button = ({onClick, text}) => {
     return (
         <button
