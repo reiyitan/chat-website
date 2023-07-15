4d698efffe2b81bf322de713414b2a5cab02f6d3
reiyitan <tanreiyi@gmail.com>
2023-07-15 12:41:34 -0500

    exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Message } from "./Message";
 export { TextEntry } from "./TextEntry";
 export { TextForm } from "./TextForm"; 
 export { Button } from "./Button";
+export { Warning } from "./Warning";
