fe4f960ee2f45d9086de16812b27c84aea5a4d6b
Rei Tan <tanreiyi@gmail.com>
2023-07-15 20:21:48 -0500

    update exports - no longer using Warning component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { Message } from "./Message";
 export { TextEntry } from "./TextEntry";
 export { TextForm } from "./TextForm"; 
 export { Button } from "./Button";
-export { Warning } from "./Warning";
