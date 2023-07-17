5cb3fe28423916516ef0764734464d34212dbd66
Rei Tan <tanreiyi@gmail.com>
2023-07-17 14:44:08 -0500

    new font

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import { ChatPage, SignInPage, SignUpPage } from "../pages";
 import "./App.css";
+import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "../env";
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export const App = () => {
+    const [user, setUser] = useState(null);
     return (
-        <SignInPage app={app}/>
+        <SignUpPage auth={auth} user={user} setUser={setUser} />
     );
 }
